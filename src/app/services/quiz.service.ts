fe42b416a9555b4a8893e9de2e03ea5fa4bc98f0
Mohamed omar BOUCHKATI <bochkati.omar@gmail.com>
2020-07-21 12:07:43 +0100

    global data

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -13,7 +13,10 @@ export class QuizService {
     { id: "csharp", name: "C Sharp", desc:"", limitTemps:"30", moyDeb:0,moyConf:0,moyExp:0 },
     { id: "designPatterns", name: "Design Patterns", desc:"", limitTemps:"30", moyDeb:0,moyConf:0,moyExp:0 }
   ];
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    if (localStorage.getItem('quizList') != undefined)
+      localStorage.setItem('quizList', JSON.stringify(this.quizList));
+  }
 
   get(type: string): any {
     let data: any;
@@ -30,10 +33,15 @@ export class QuizService {
   }
 
   getAll() {
-    return this.quizList;
+    if (localStorage.getItem('quizList') != undefined)
+       return JSON.parse(localStorage.getItem('quizList'));
+    else
+      return this.quizList;
   }
 
   addQuizz(data) {
+    let tempdata = JSON.parse(localStorage.getItem('quizList')).push(data);
+    localStorage.setItem('quizList', JSON.stringify(tempdata))
     this.quizList.push(data)
 }
 }
